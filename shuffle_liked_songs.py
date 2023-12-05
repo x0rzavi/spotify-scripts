f756f5c0f28079600578fff8e4cae9c489dd9fa5
Avishek Sen <x0rzavi@gmail.com>
2023-12-05 20:45:16 +0530

    add liked songs shuffler

---
+++ shuffle_liked_songs.py
@@ -0,0 +1,59 @@
+"""
+Shuffle Spotify Liked Songs into a Playlist
+"""
+
+import random
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+scope = "playlist-read-private playlist-modify-private user-library-read"
+client_id = os.environ.get("SPOTIPY_CLIENT_ID")
+client_secret = os.environ.get("SPOTIPY_CLIENT_SECRET")
+redirect_uri = os.environ.get("SPOTIPY_REDIRECT_URI")
+auth_manager = SpotifyOAuth(
+    client_id=client_id,
+    client_secret=client_secret,
+    redirect_uri=redirect_uri,
+    scope=scope,
+    open_browser=False,
+)
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+
+def new_playlist():
+    playlists = sp.current_user_playlists()
+    for playlist in playlists["items"]:
+        if "Liked Songs Playlist" in playlist["name"]:
+            sp.current_user_unfollow_playlist(playlist["id"])
+            break
+    playlist_name = "Liked Songs Playlist"
+    playlist_description = "A playlist of liked songs"
+    playlist = sp.user_playlist_create(
+        user_id, playlist_name, public=False, description=playlist_description
+    )
+    return playlist["id"]
+
+
+user_id = sp.me()["id"]
+playlist_id = new_playlist()
+
+
+def get_tracks(songs_list):
+    tracks_list = []
+    while True:
+        for item in songs_list["items"]:
+            track = item["track"]
+            track_uri = track["uri"]
+            tracks_list.append(track_uri)
+        if songs_list["next"]:
+            songs_list = sp.next(songs_list)
+        else:
+            break
+    return tracks_list
+
+
+liked_songs_uris = get_tracks(sp.current_user_saved_tracks())
+random.shuffle(liked_songs_uris)
+for i in range(0, len(liked_songs_uris), 100):
+    sp.user_playlist_add_tracks(user_id, playlist_id, liked_songs_uris[i : i + 100])
